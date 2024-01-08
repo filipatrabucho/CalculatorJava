e4133bd15ef874ecf01cd877a9d5a5e99de0c530
Ana Filipa <98351447+filipatrabucho@users.noreply.github.com>
2024-01-08 14:05:37 +0000

    first commit

---
+++ src/tarefacalculadora/TarefaCalculadora.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarefacalculadora;
+
+/**
+ *
+ * @author filip
+ */
+public class TarefaCalculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          new Classe_Janela().setVisible(true);
+    }
+    
+}
